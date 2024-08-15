c2bc7ec8d51ddd6fc06ef381b27345034d76592b
Samim MBT <samimmbt@gmail.com>
2024-08-15 23:27:35 +0330

    Creating a form

---
+++ src/Form/UserSearchType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('username', TextType::class, [
+            'label' => 'Search for a user',
+            'attr' => ['placeholder' => 'Enter username']
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
