c4b110b266749035a2395e0fc6df1173e3c56c65
Samim MBT <samimmbt@gmail.com>
2024-08-17 22:12:00 +0330

    Entitiy manager and new logic for Home route

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserSearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,6 +20,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'index')]
     public function index(): Response
     {
@@ -24,9 +35,32 @@ class RegistrationController extends AbstractController
     }
 
     #[Route('/home', name: 'home')]
-    public function home(): Response
+    public function home(Request $request, LoggerInterface $logger): Response
     {
-        return $this->render('game/index.html.twig', ['game' => null, 'rank' => null, 'error' => null]);
+        $form = $this->createForm(UserSearchType::class,null,['attr' => ['id' => 'form']]);
+        $form->handleRequest($request);
+        $users = [];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $users = $this->entityManager->getRepository(User::class)->searchUsers($data['username']);            
+            $logger->info("Form submitted:", [// Log the search results
+                'form' => $data,
+                'users' => $users, // Log all users found
+            ]);
+            $userArray = [];
+        foreach ($users as $user) {
+            $userArray[] = [
+                'id' => $user->getId(),          // Get user ID
+                'username' => $user->getUsername() // Get username
+            ];
+            $logger->info('User found: ' . $user->getUsername() . "data:" . $data['username'],['Data'=>$userArray]);
+        }
+
+        return $this->redirectToRoute('home', ['users' => $userArray]);
+            // return new JsonResponse(['users'=>$users]);
+        }
+        
+        return $this->render('game/index.html.twig', ['game' => null, 'rank' => null, 'error' => null, 'users' => $users, 'form' => $form->createView()]);
     }
 
     #[Route('/signup', name: 'app_register')]
@@ -50,7 +84,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('registration/register.html.twig', [
