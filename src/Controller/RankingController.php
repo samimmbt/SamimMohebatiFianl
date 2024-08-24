97709a993339819353aa39099ff174ff400f8a01
Samim MBT <samimmbt@gmail.com>
2024-08-24 14:37:39 +0330

    A new Controller for Ranks

---
+++ src/Controller/RankingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class RankingController extends AbstractController
+{
+    #[Route('/ranking', name: 'app_ranking')]
+    public function index(UserRepository $userRepository): JsonResponse
+    {
+        $users = $userRepository->findTopUsersByWins(10); // Fetch top 10 users by wins
+
+        $rankingData = array_map(function ($user) {
+            return [
+                'number' => $user->getId(),
+                'name' => $user->getUsername(),
+                'wins' => $user->getWins(),
+            ];
+        }, $users);
+
+        return $this->json(['success' => true, 'data' => $rankingData]);
+    }
+}
